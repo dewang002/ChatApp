54790b8aa927c516a35bad546e3b3fc4137f0f54
Devansh Rajvanshi <devanshrajvanshi@192.168.1.9>
2025-03-01 14:08:12 +0530

    done

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,29 @@
-export const signup = ( res, req ) => {
-    res.send('signup')
+import User from "../models/auth.modal.js";
+import bcrypt from 'bcryptjs'
+
+export const signup = async ( res, req ) => {
+    const {fullName,password,email} = req.body;
+    try{
+        if(password.length<6){
+            return res.status(400).json({msg:'please enter the valid password'})
+        }
+        const user = await User.findOne({email})
+        if(user)res.status(400).json({msg:'this user already exist'})
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.genSalt.hash(password,salt)
+        const newUser = await new User({
+            email,
+            password:hashedPassword,
+            fullName
+        })
+        if(newUser){
+
+        }else{
+            res.status(400).json({msg:'please enter right creadentials'})
+        }
+    }catch(err){
+
+    }
 }
 export const login = ( res, req ) => {
     res.send('signup')
